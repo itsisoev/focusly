c8e80f924d5cd09c13d7625ed903a73173639585
Daler <isoev9733@gmail.com>
2025-07-15 11:12:22 +0500

    Берем задний фон из локального хранилище

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
 import {TuiRoot} from "@taiga-ui/core";
-import {Component} from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot],
+  imports: [RouterOutlet, TuiRoot, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'frontend';
+export class App implements OnInit {
+  imgBackground = signal<string>('');
+
+  ngOnInit() {
+    this.loadBackgroundFromStorage();
+  }
+
+  private loadBackgroundFromStorage() {
+    const imagePath = localStorage.getItem('selected_gradient');
+    if (imagePath) {
+      this.imgBackground.set(imagePath);
+    }
+  }
 }
